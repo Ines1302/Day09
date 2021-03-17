bdc3b6b85d6bf715e47a65b3af23aece0428ea77
Ines1302 <seeking.knowledge13@gmail.com>
2021-03-17 02:18:49 +0100

    Create index.js

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,11 +1,10 @@
 function functionElse(num) {
   var result;
   if (num <= 5) {
-    result = "5 or smaller";
+    return (result = "5 or smaller");
   } else {
-    result = "Bigger than 5";
+    return (result = "Bigger than 5");
   }
-  return result;
 }
 functionElse(4);
 functionElse(5);
